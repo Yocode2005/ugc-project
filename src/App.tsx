3ef21d3b621e1c8333aa8731662bb1c3c4eba2b9
Yocode2005 <yogeshchhandhwal2005@gmail.com>
2026-04-28 20:15:29 +0530

    import navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import Navbar from './components/Navbar';
+import Navbar from './components/Navbar'; //  import navbar compnent
 import Home from './pages/Home';
 import SoftBackdrop from './components/SoftBackdrop';
 import Footer from './components/Footer';
